68bb673a355db91d83ce49dcf26d2f63f339921e
Mohammad Saif <mohammadsaif1935@gmail.com>
2023-12-28 09:43:21 +0300

    Added demo clips

--- lib/database_access/services/news_services.dart
+++ lib/database_access/services/news_services.dart
@@ -7,20 +7,24 @@ import '../problem_domains/news.dart';
 
 class NewsServices {
   Future<List<News>> fetchNews() async {
-    // Fetching the data from server
-    http.Response response = await http.get(NEWS_API);
-
-    // Decoding data we fetched previously
-    Map<String, dynamic> responseObj =
-        json.decode(response.body) as Map<String, dynamic>;
-    List responseNewsObj = responseObj['articles'];
     List<News> _newsList = [];
-    // Assigning the news data to the list for bloc
-    if (responseObj.isNotEmpty) {
-      for (var newsData in responseNewsObj) {
-        News _news = News.fromJson(newsData);
-        _newsList.add(_news);
+    try {
+      // Fetching the data from server
+      http.Response response = await http.get(NEWS_API);
+
+      // Decoding data we fetched previously
+      Map<String, dynamic> responseObj =
+          json.decode(response.body) as Map<String, dynamic>;
+      List responseNewsObj = responseObj['articles'];
+      // Assigning the news data to the list for bloc
+      if (responseObj.isNotEmpty) {
+        for (var newsData in responseNewsObj) {
+          News _news = News.fromJson(newsData);
+          _newsList.add(_news);
+        }
       }
+    } catch (e) {
+      print(e);
     }
     return _newsList;
   }
